9290188df7637283b02b24864740a989294d60dd
TonyGunk01 <rishivl@hotmail.com>
2025-12-18 14:20:32 +0530

    Creating instances of Commands
    Created an instance of the required unit command inside InputService.
    
    Created CommandData method to return newly created command data.
    
    Created a new Unit Command using this instance of CommandData.
    
    Passed the created unit command to GameService for further processing.

--- Assets/Scripts/Input/InputService.cs
+++ Assets/Scripts/Input/InputService.cs
@@ -1,6 +1,7 @@
+using Assets.Scripts.Commands;
+using Command.Actions;
 using Command.Main;
 using Command.Player;
-using Command.Actions;
 
 namespace Command.Input
 {
@@ -48,7 +49,45 @@ namespace Command.Input
         public void OnTargetSelected(UnitController targetUnit)
         {
             SetInputState(InputState.EXECUTING_INPUT);
-            GameService.Instance.PlayerService.PerformAction(selectedCommandType, targetUnit);
+            UnitCommand commandToProcess = CreateUnitCommand(targetUnit);
+            GameService.Instance.ProcessUnitCommand(commandToProcess);
+        }
+
+        private CommandData CreateCommandData(UnitController targetUnit)
+        {
+            return new CommandData(GameService.Instance.PlayerService.ActiveUnitID, targetUnit.UnitID, GameService.Instance.PlayerService.ActivePlayerID, targetUnit.Owner.PlayerID);
+        }
+
+        private UnitCommand CreateUnitCommand(UnitController targetUnit)
+        {
+            CommandData commandData = CreateCommandData(targetUnit);
+
+            switch (selectedCommandType)
+            {
+                case CommandType.Attack:
+                    return new AttackCommand(commandData);
+
+                case CommandType.Heal:
+                    return new HealCommand(commandData);
+
+                case CommandType.AttackStance:
+                    return new AttackStanceCommand(commandData);
+
+                case CommandType.Cleanse:
+                    return new CleanseCommand(commandData);
+
+                case CommandType.BerserkAttack:
+                    return new BerserkAttackCommand(commandData);
+
+                case CommandType.Meditate:
+                    return new MeditateCommand(commandData);
+
+                case CommandType.ThirdEye:
+                    return new ThirdEyeCommand(commandData);
+
+                default:
+                    throw new System.Exception($"No Command found of type: {selectedCommandType}");
+            }
         }
     }
 }
