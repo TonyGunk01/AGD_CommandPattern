e3cf11ff5340eb605051c5008c83b3b7ffc6bd2c
TonyGunk01 <rishivl@hotmail.com>
2025-12-19 13:34:46 +0530

    Creating Replay Service
    Created /Scripts/Replay/ folder and create ReplayService.cs in it
    
    Created Replay Service in GameService
    
    Created a method to update this stack
    
    Created a method to execute the next command in replay stack if the replay state is active
    
    Added a delay of 1 second before executing the next command.

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -13,16 +13,8 @@ using Command.Commands;
 
 namespace Command.Main
 {
-
-    /**  This script demonstrates implementation of the Service Locator Pattern.
-    *  If you're interested in learning about Service Locator Pattern, 
-    *  you can find a dedicated course on Outscal's website.
-    *  Link: https://outscal.com/courses
-    **/
-
     public class GameService : GenericMonoSingleton<GameService>
     {
-        // Services:
         public EventService EventService { get; private set; }
         public SoundService SoundService { get; private set; }
         public ActionService ActionService { get; private set; }
@@ -30,15 +22,14 @@ namespace Command.Main
         public BattleService BattleService { get; private set; }
         public PlayerService PlayerService { get; private set; }
         public CommandInvoker CommandInvoker { get; private set; }
+        public ReplayService ReplayService { get; private set; }
 
         [SerializeField] private UIService uiService;
         public UIService UIService => uiService;
 
-        // Scriptable Objects:
         [SerializeField] private SoundScriptableObject soundScriptableObject;
         [SerializeField] private List<BattleScriptableObject> battleScriptableObjects;
 
-        // Scene References:
         [SerializeField] private AudioSource sfxSource;
         [SerializeField] private AudioSource bgMusicSource;
 
@@ -52,6 +43,7 @@ namespace Command.Main
             PlayerService = new PlayerService();
             uiService.Init(battleScriptableObjects.Count);
             CommandInvoker = new CommandInvoker();
+            ReplayService = new ReplayService();
         }
 
         private void Update() => InputService.UpdateInputService();
---
+++ Assets/Scripts/Replay/ReplayService.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplayService
+{
+    private Stack<ICommand> replayCommandStack;
+    public ReplayState ReplayState { get; private set; }
+    public ReplayService() => SetReplayState(ReplayState.DEACTIVE);
+    public void SetReplayState(ReplayState stateToSet) => ReplayState = stateToSet;
+    public void SetCommandStack(Stack<ICommand> commandsToSet) => replayCommandStack = new Stack<ICommand>(commandsToSet);
+
+    public void ExecuteNext()
+    {
+        if (replayCommandStack.Count > 0)
+            GameService.Instance.ProcessUnitCommand(replayCommandStack.Pop());
+    }
+}
